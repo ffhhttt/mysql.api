32c4890b247ed38ef36db54205ab28e46a7a352b
f ff <mf21150052@smail.nju.edu.cn>
2022-05-10 18:08:36 +0800

    解决ID自增时ID跳跃问题

--- src/main/java/com/example/demo/service/ExperimentlogRepository.java
+++ src/main/java/com/example/demo/service/ExperimentlogRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Experimentlog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface ExperimentlogRepository extends JpaRepository<Experimentlog,Int
     List<Experimentlog> findByExperimentsId(Integer experimentsId);
     List<Experimentlog> findByUsersId(Integer usersId);
 
+    @Query(value = "select max(id) from experimentlog;", nativeQuery = true)
+    Integer getMaxID();
 }
